8d046efd5f7bc490f5f17c2604c306eb27fc2a31
Arthur Gauss <gauss.ind@gmail.com>
2021-05-23 17:24:41 +0300

    Added some Variable for Reviewer and Student

--- share_class.py
+++ share_class.py
@@ -8,6 +8,15 @@ class Student:
         self.grades = {}
 
 
+    def rate_lw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -31,6 +40,9 @@ class Reviewer(Mentor):
     def __str__(self):
        return "Имя: " + self.name + '\nФамилия:' + self.surname
 
+
+
+
 class Lecturer(Mentor):
     def __init__(self, name, surname, average_value):
         Mentor.__init__(self, name, surname)
@@ -43,13 +55,17 @@ class Lecturer(Mentor):
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_reviewer = Reviewer('Some', 'Buddy')
-cool_reviewer.courses_attached += ['Python']
+basic_student = Student('Johar', 'Buzuluk', 'helicopter')
+basic_student.courses_in_progress += ['Git']
+
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 
+basic_reviewer = Reviewer('Slender' , 'Man')
+basic_reviewer.courses_attached += ['Git']
+basic_reviewer.rate_hw(basic_student, 'Git', 7)
 
 cool_lecturer = Lecturer('Son', 'Tzy', '7')
 cool_lecturer.courses_attached += ['Art of War']
