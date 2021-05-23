0fe25d4c550f88325d0a30c40cc48dfbe8a77950
Arthur Gauss <gauss.ind@gmail.com>
2021-05-23 12:54:14 +0300

    Added Class Lecturer

--- share_class.py
+++ share_class.py
@@ -7,7 +7,8 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-#blblkavla
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -17,7 +18,7 @@ class Mentor:
 
 
 
-class Viewer(Mentor):
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -27,22 +28,36 @@ class Viewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+       return "Имя: " + self.name + '\nФамилия:' + self.surname
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname, average_value):
+        Mentor.__init__(self, name, surname)
+        self.average_value = average_value
+    def __str__(self):
+        return "Имя: " + self.name + '\nФамилия:' + self.surname + '\nСредняя оценка за лекции: ' + self.average_value
+
+
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-cool_viewer = Viewer('Some', 'Buddy')
-cool_viewer.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-#todo Upadte something
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+cool_reviewer.rate_hw(best_student, 'Python', 10)
 
 
-cool_viewer.rate_hw(best_student, 'Python', 10)
+cool_lecturer = Lecturer('Son', 'Tzy', '7')
+cool_lecturer.courses_attached += ['Art of War']
+
+
+
+
 
 print(best_student.grades)
+print(cool_reviewer)
+print(cool_lecturer)
