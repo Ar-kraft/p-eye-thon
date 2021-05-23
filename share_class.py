0788dffd671112ae8d3382470bcb3a5f15525076
Arthur Gauss <gauss.ind@gmail.com>
2021-05-23 21:36:14 +0300

    Print out of Lecture Review

--- share_class.py
+++ share_class.py
@@ -7,15 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-
-    def rate_lw(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
-            if course in lecturer.grades:
-                lecturer.grades[course] += [grade]
+    def rate_lw(self, lecturer, course, rate):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.rates:
+                lecturer.rates[course] += [rate]
             else:
-                lecturer.grades[course] = [grade]
+                lecturer.rates[course] = [rate]
         else:
             return 'Ошибка'
+
 
 
 class Mentor:
@@ -44,36 +44,48 @@ class Reviewer(Mentor):
 
 
 class Lecturer(Mentor):
-    def __init__(self, name, surname, average_value):
+    def __init__(self, name, surname):
         Mentor.__init__(self, name, surname)
-        self.average_value = average_value
+        self.rates = {}
     def __str__(self):
-        return "Имя: " + self.name + '\nФамилия:' + self.surname + '\nСредняя оценка за лекции: ' + self.average_value
+        return "Имя: " + self.name + '\nФамилия:' + self.surname + '\n Рейтинг'
 
 
+
+
+
+cool_lecturer = Lecturer('Son', 'Tzy' )
+cool_lecturer.courses_attached += ['Git']
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-basic_student = Student('Johar', 'Buzuluk', 'helicopter')
+basic_student = Student('John', 'Buzuluk', 'helicopter')
 basic_student.courses_in_progress += ['Git']
+basic_student.rate_lw(cool_lecturer, 'Git', 13)
 
+foreign_student = Student('Ahmed', 'Kugelbaum', 'cyborg')
+foreign_student.courses_in_progress += ['Git']
+foreign_student.rate_lw(cool_lecturer, 'Git', 15)
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 
-basic_reviewer = Reviewer('Slender' , 'Man')
-basic_reviewer.courses_attached += ['Git']
-basic_reviewer.rate_hw(basic_student, 'Git', 7)
 
-cool_lecturer = Lecturer('Son', 'Tzy', '7')
-cool_lecturer.courses_attached += ['Art of War']
+def avg(x):
+    return sum(x) / len(x)
 
 
 
 
-
+print(basic_student.grades)
 print(best_student.grades)
+
 print(cool_reviewer)
 print(cool_lecturer)
+
+
+
+print(cool_lecturer.rates)
+
