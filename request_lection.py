81a0a15b9257512498de800321194e133214b0fe
Arthur Gauss <gauss.ind@gmail.com>
2021-07-05 12:12:53 +0300

    requets lection

---
+++ request_lection.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+
+import requests
+
+def test_request():
+    url = "https://httpbin.org/get"
+    params = {"model": "nike321"}
+    headers = {"Authorization": "secret - token - 123"}
+    timeout = 5
+    response = requests.get(url, params=params, headers=headers, timeout=timeout)
+    pprint(response)
+    pprint(response.json())
+
+if __name__ == '__main__':
+    test_request()
+
